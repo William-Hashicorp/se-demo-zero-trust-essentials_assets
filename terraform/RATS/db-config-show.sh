4bccfa1e69bfc8729475e792863094578b9b0409
William Yang <william.yang@hashicorp.com>
2023-01-14 00:44:06 +0800

    add files

--- terraform/RATS/db-config-show.sh
+++ terraform/RATS/db-config-show.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd /root/terraform
+# cd /root/terraform
 
-TF_PRODUCT_API_HOST=$(cd /root/terraform && terraform output -json target_ec2_attributes | jq -r .product_api.ip)
+TF_PRODUCT_API_HOST=$(terraform output -json target_ec2_attributes | jq -r .product_api.ip)
 
 while true; do
-  DB_CONFIG=$(ssh -i private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PRODUCT_API_HOST cat /opt/hashicups/product-api-go/conf.json)
+  DB_CONFIG=$(ssh -i ~/.ssh/private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PRODUCT_API_HOST cat /opt/hashicups/product-api-go/conf.json)
   clear
   echo "$DB_CONFIG"
   sleep 5
--- terraform/RATS/network-monitor.sh
+++ terraform/RATS/network-monitor.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd /root/terraform
+#cd /root/terraform
 
-TF_PUBLIC_API_HOST=$(cd /root/terraform && terraform output -json target_ec2_attributes | jq -r .public_api.ip)
+TF_PUBLIC_API_HOST=$(terraform output -json target_ec2_attributes | jq -r .public_api.ip)
 
-ssh -i private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo apt install -y ngrep
-ssh -t -i private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo ngrep -q -d eth0 "" port 8081
+ssh -i ~/.ssh/private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo apt install -y ngrep
+
+ssh -t -i ~/.ssh/private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo ngrep -q -d eth0 "" port 8081 or port 21000 or port 21001 or port 21002 or port 18081
+
+#ssh -i ~/.ssh/private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo apt install -y nigrep
+#ssh -t -i ~/.ssh/private.key -o "StrictHostKeyChecking=false" ubuntu@$TF_PUBLIC_API_HOST sudo ngrep -q -d eth0 "" port 8081
